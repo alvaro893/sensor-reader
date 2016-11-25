8b21aec3ef900fd93079548006420a030ddc244f
Álvaro Bolaños Rodríguez <alvarobolro@gmail.com>
2016-11-25 17:12:43 +0200

    fix bug in serial communication

--- SerialCommunication.py
+++ SerialCommunication.py
@@ -9,6 +9,7 @@ if os.name == WINDOWS:
     port = PORT_WINDOWS[0]
 sequence = INITIAL_SEQUENCE
 ser = serial.Serial(port, BAUD_SPEED)
+print("serial port:", port, " ", BAUD_SPEED, " ", os.name)
 
 def readHex(data):
     print(' '.join(x.encode('hex') for x in data))
@@ -16,7 +17,8 @@ def readHex(data):
 
 def start(process_line):
     #ser.write("*IDN?\n") # this solves the issue of not reading in linux
-    while True:
+    ser.open()
+    while ser.is_open:
         data = ser.read_until(sequence)
         readHex(data)
         process_line(bytearray(data))
