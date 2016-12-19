84430805a987a7768e369091c934a922c9464ef0
alvaro893 <alvarobolro@gmail.com>
2016-12-19 12:53:10 +0200

    add haardcascade files and fix detect serial on windows# On branch master

--- Haarcascade.py
+++ Haarcascade.py
@@ -2,22 +2,48 @@ import cv2
 import numpy as np
 __author__ = 'Alvaro'
 
-body_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_upperbody.xml')
+import cv2
+import time
 
-cap = cv2.VideoCapture(0)
+face_cascade = cv2.CascadeClassifier("haarcascades/haarcascade_frontalface_default.xml")
+cap = cv2.VideoCapture(0) #Open video file
 
-while True:
-    ret, img = cap.read()
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    bodies = body_cascade.detectMultiScale(gray, 1.3, 5)
+w = cap.get(3) #get width
+h = cap.get(4) #get height
 
-    print len(bodies)
-    for (x,y,w,h) in bodies:
-        cv2.rectangle(img, (x,y), (x+w, y+h), (255,0,0), 2)
-        roi_gray = gray[y:y+h, x:x+w]
-        roi_color = img[y:y+h, x:x+w]
+mx = int(w/2)
+my = int(h/2)
 
-    cv2.imshow('img', img)
-    k = cv2.waitKey(50) & 0xff
-    if k == ord('q'):
+lasttime = 0
+count = 0
+while(cap.isOpened()):
+    count +=1
+    ret, frame = cap.read() #read a frame
+    try:
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+        for (x,y,w,h) in faces:
+            cv2.rectangle(gray, (x,y), (x+w, y+h), (255), 3)
+            cv2.putText(gray, "face", (x,y),cv2.FONT_HERSHEY_SIMPLEX
+                    ,1,(255,255,255),1,cv2.LINE_AA)
+    except Exception as e:
+        print e.message
+    try:
+        text = '%.2f fps' % (count/time.clock())
+        cv2.putText(frame, text ,(mx,my),cv2.FONT_HERSHEY_SIMPLEX
+                    ,1,(0,0,255),1,cv2.LINE_AA)
+        cv2.imshow('Frame',gray)
+    except:
+        #if there are no more frames to show...
+        print('EOF')
         break
+
+    #Abort and exit with 'Q' or ESC
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
+        break
+    if k == ord('w'):
+        time.sleep(2)
+
+cap.release() #release video file
+cv2.destroyAllWindows() #close all openCV windows
