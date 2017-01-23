a2d24a37d49ff77a0cf9664b2debb8df8b7395cb
Álvaro Bolaños Rodríguez <alvarobolro@gmail.com>
2017-01-23 21:16:17 +0200

    websocket connection working

--- Constants.py
+++ Constants.py
@@ -5,6 +5,9 @@ __author__ = AUTHOR
 # URL = "https://ir-sensor-cloud.appspot.com/"
 URL = "http://server-ir-cloud.44fs.preview.openshiftapps.com/"
 URL_DEBUG = "http://localhost:5000"
+WS_URL = "ws://localhost"
+CAMERA_PATH = "/camera"
+CLIENT_PATH = "/client"
 KEY = "development_server"
 
 PORT_LINUX = '/dev/ttyACM0', '/dev/ttyACM1'
