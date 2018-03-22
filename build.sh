71ba356499eb4f8d9d730d60fec3829f924b110a
Álvaro Bolaños Rodríguez <alvarobolro@gmail.com>
2018-03-22 13:23:29 +0200

    update service files

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+# Note: the '|& tee -a "$logfile"'  line copies the ouput to both screen and logfile
 workfolder="/home/pi/sensor-reader"
 logfile="$workfolder/build.log"
 service_files_path="$workfolder/service_files"
 
-echo "clean Log file and add to log"
+echo "clean Log file and add to log\n\n" |& tee -a "$logfile"
 echo "" > "$logfile"
 echo "-------------------------------------" >> "$logfile"
 
@@ -11,20 +12,23 @@ echo "update time"
 "$service_files_path/synctime.sh"
 date >> "$logfile"
 
-echo "make all files in service_files executable..."
+echo "make all files in service_files executable...\n\n" |& tee -a "$logfile"
 chmod -R +x "$service_files_path"
 
 
-echo "run main script and log it"
+echo "run main script and log it\n\n" |& tee -a "$logfile"
 "$service_files_path/main-script.sh" |& tee -a "$logfile"
 
-echo "check if we need to install opencv"
-python -c 'import cv2' || echo "need to install openCV" && "$service_files_path/install-opencv.sh" |& tee -a "$logfile"
+echo "check if we need to pytinstall opencv"
+python -c 'import cv2' || "$service_files_path/install-opencv.sh" |& tee -a "$logfile"
 
 
-echo "Build Cython code..."
+echo "Build Cython code...\n\n" |& tee -a "$logfile"
 cd analysis
 sudo python setup.py build_ext -b ..
 cd ..
+
+echo "Restart sensor service\n\n"  |& tee -a "$logfile"
+"$service_files_path/reset-sensor.sh" |& tee -a "$logfile"
 
 echo "------Build.sh script done" |& tee -a "$logfile"
--- service_files/install-opencv.sh
+++ service_files/install-opencv.sh
@@ -3,5 +3,5 @@ echo "installing openCV 3.1.0"
 sudo apt-get install libtiff5-dev libjasper-dev libpng12-dev libavcodec-dev \
 libavformat-dev libswscale-dev libv4l-dev libatlas-base-dev gfortran libgtk2.0-dev -y && \
 wget "https://github.com/jabelone/OpenCV-for-Pi/raw/master/latest-OpenCV.deb" && \
-sudo dpkg -i latest-OpenCV.deb && /
+sudo dpkg -i latest-OpenCV.deb && \
 rm -f latest-OpenCV.deb
